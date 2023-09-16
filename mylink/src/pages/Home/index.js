2bda17d7812b861f83a03749111b289da3f957ef
brunowcst <brunoporfirio@gmail.com>
2023-09-16 20:40:32 -0300

    Using component statusBar

--- mylink/src/pages/Home/index.js
+++ mylink/src/pages/Home/index.js
@@ -1,10 +1,18 @@
-import { View, Text } from 'react-native'
+import { View, Text} from 'react-native'
 import React from 'react'
+import StatusBarPage from '../../components/StatusBar'
+import Menu from '../../components/Menu'
 
 export default function Home() {
   return (
-    <View>
+    <View style={{flex:1, backgroundColor:'#222'}}>
+      <Menu/> 
       <Text>Home</Text>
+      <StatusBarPage
+        barStyle='light-content'
+        backgroundColor='#222'
+      />
+
     </View>
   )
 }
