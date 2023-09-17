2e337c727934cea60ef113e6f4b578a01ddfaf5c
Brunowcst <brunoporfirio022@gmail.com>
2023-09-17 14:34:50 -0300

    keybord dismiss function

--- mylink/src/pages/Home/index.js
+++ mylink/src/pages/Home/index.js
@@ -1,7 +1,9 @@
-import { View, Text} from 'react-native'
+import { View, TouchableWithoutFeedback, Keyboard} from 'react-native'
 import React from 'react'
+
 import StatusBarPage from '../../components/StatusBar'
 import Menu from '../../components/Menu'
+
 import { ContainerContent, Title, SubTitle, ContainerInput, Input, BoxIcon, ButtonLink, ButtonLinkText } from './styles'
 
 import Icon from 'react-native-vector-icons/Feather'
@@ -9,53 +11,43 @@ import IconF from 'react-native-vector-icons/FontAwesome5'
 
 export default function Home() {
   return (
-    <View style={{flex:1, backgroundColor:'#222', position:'relative', alignItems: 'center'}}>
-      <StatusBarPage
-        barStyle='light-content'
-        backgroundColor='#222'
-      />
-      <Menu/> 
-
-      <IconF 
-        name='external-link-alt' 
-        size={100} 
-        color={'#5C1374'}
-        style={{marginTop: 80}}
-      />
-
-      <ContainerContent>
-        <Title>
-          EncurtaLinks
-        </Title>
-
-        <SubTitle>
-          Cole seu link para encurtar:
-        </SubTitle>
-
-      </ContainerContent>
-
-      <ContainerInput>
-          <BoxIcon>
-            <Icon name='link' color={'#fff'} size={18}/>
-          </BoxIcon>
-          <Input
-            style={{color:'#fff'}}
-            placeholder='Cole seu link aqui'
-            placeholderTextColor={'rgba(255, 255, 255, 0.60)'}
-          />
-      </ContainerInput>
-
-      <ButtonLink>
-        <ButtonLinkText>
-          Gerar link
-        </ButtonLinkText>
-      </ButtonLink>
-
-      <Text>
-        Teste
-      </Text>
-      
-
-    </View>
+    <TouchableWithoutFeedback onPress={() => Keyboard.dismiss()}>
+      <View style={{flex:1, backgroundColor:'#222', position:'relative', alignItems: 'center'}}>
+        <StatusBarPage
+          barStyle='light-content'
+          backgroundColor='#222'
+        />
+        <Menu/>
+        <IconF
+          name='external-link-alt'
+          size={100}
+          color={'#5C1374'}
+          style={{marginTop: 80}}
+        />
+        <ContainerContent>
+          <Title>
+            EncurtaLinks
+          </Title>
+          <SubTitle>
+            Cole seu link para encurtar:
+          </SubTitle>
+        </ContainerContent>
+        <ContainerInput>
+            <BoxIcon>
+              <Icon name='link' color={'#fff'} size={18}/>
+            </BoxIcon>
+            <Input
+              style={{color:'#fff'}}
+              placeholder='Cole seu link aqui'
+              placeholderTextColor={'rgba(255, 255, 255, 0.60)'}
+            />
+        </ContainerInput>
+        <ButtonLink>
+          <ButtonLinkText>
+            Gerar link
+          </ButtonLinkText>
+        </ButtonLink>
+      </View>
+    </TouchableWithoutFeedback>
   )
 }
