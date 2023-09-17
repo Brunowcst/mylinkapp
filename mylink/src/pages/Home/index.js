afb67babd628ad1e340342cbd95ee02b6fedf497
Brunowcst <brunoporfirio022@gmail.com>
2023-09-17 17:53:16 -0300

    Modal in home

--- mylink/src/pages/Home/index.js
+++ mylink/src/pages/Home/index.js
@@ -1,8 +1,9 @@
-import { View, TouchableWithoutFeedback, Keyboard, KeyboardAvoidingView, Platform} from 'react-native'
-import React from 'react'
+import { View, TouchableWithoutFeedback, Keyboard, KeyboardAvoidingView, Platform, Modal } from 'react-native'
+import React, { useState } from 'react'
 
 import StatusBarPage from '../../components/StatusBar'
 import Menu from '../../components/Menu'
+import CustomModal from '../../components/CustomModal'
 
 import { ContainerContent, Title, SubTitle, ContainerInput, Input, BoxIcon, ButtonLink, ButtonLinkText } from './styles'
 
@@ -10,61 +11,72 @@ import Icon from 'react-native-vector-icons/Feather'
 import IconF from 'react-native-vector-icons/FontAwesome5'
 
 export default function Home() {
+
+  const [linkValue, setLinkValue] = useState('');
+  const [modalVisible, setModalVisible] = useState(false);
+
+  function handleShortLink() {
+    // alert('Link:' + linkValue)
+    setModalVisible(true)
+  }
+
   return (
     <TouchableWithoutFeedback onPress={() => Keyboard.dismiss()}>
-      <View style={{flex:1, backgroundColor:'#222', alignItems: 'center'}}>
+      <View style={{ flex: 1, backgroundColor: '#222'}}>
 
-        <StatusBarPage
-          barStyle='light-content'
-          backgroundColor='#222'
-        />
+          <StatusBarPage barStyle='light-content' backgroundColor='#222'/>
 
-        <Menu/>
+          <Menu />
 
-        <KeyboardAvoidingView
-          behavior={Platform.OS === 'android' ? 'position' : 'padding'}
-          enabled
-          keyboardVerticalOffset={10}
-        >
-          <IconF
-            name='external-link-alt'
-            size={100}
-            color={'#5C1374'}
-            style={{paddingTop: 80, alignSelf:'center'}}
-          />
+          <KeyboardAvoidingView
+            behavior={Platform.OS === 'android' ? 'position' : 'padding'}
+            enabled
+            keyboardVerticalOffset={10}
+          >
+            <IconF
+              name='external-link-alt'
+              size={100}
+              color={'#5C1374'}
+              style={{ paddingTop: 80, alignSelf: 'center' }}
+            />
 
-          <ContainerContent>
-            <Title>
-              EncurtaLinks
-            </Title>
-            <SubTitle>
-              Cole seu link para encurtar:
-            </SubTitle>
-          </ContainerContent>
+            <ContainerContent>
+              <Title>EncurtaLinks</Title>
+              <SubTitle>Cole seu link para encurtar:</SubTitle>
+            </ContainerContent>
 
-          <ContainerInput>
+            <ContainerInput>
+
               <BoxIcon>
-                <Icon name='link' color={'#fff'} size={18}/>
+                <Icon name='link' color={'#fff'} size={18} />
               </BoxIcon>
+
               <Input
-                style={{color:'#fff'}}
+                style={{ color: '#fff' }}
                 placeholder='Cole seu link aqui'
                 placeholderTextColor={'rgba(255, 255, 255, 0.60)'}
                 autoCapitalize='none'
                 autoCorrect={false}
                 keyboardType='url'
+                value={linkValue}
+                onChangeText={(text) => setLinkValue(text)}
               />
-          </ContainerInput>
+              
+            </ContainerInput>
 
-          <ButtonLink>
-            <ButtonLinkText>
-              Gerar link
-            </ButtonLinkText>
-          </ButtonLink>
+            <ButtonLink onPress={handleShortLink}>
+              <ButtonLinkText >
+                Gerar link
+              </ButtonLinkText>
+            </ButtonLink>
 
-      </KeyboardAvoidingView>
-        
+          </KeyboardAvoidingView>
+
+        <Modal transparent visible={modalVisible} animationType='slide'>
+          <CustomModal closeModal={() => setModalVisible(false)}/>
+        </Modal>
+
       </View>
     </TouchableWithoutFeedback>
-  )
+  );
 }
