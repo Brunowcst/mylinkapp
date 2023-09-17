65a9414be661ec147d4bbdaf2126d0858be25f3d
Brunowcst <brunoporfirio022@gmail.com>
2023-09-17 17:53:25 -0300

    Custom modal

---
+++ mylink/src/components/CustomModal/index.js
@@ -0,0 +1,52 @@
+import { TouchableOpacity, View } from 'react-native'
+import React from 'react'
+
+import Icon from 'react-native-vector-icons/MaterialIcons'
+import IconF  from 'react-native-vector-icons/Feather'
+import IconCopy from 'react-native-vector-icons/Octicons'
+
+import { ModalContainer, Container, HeaderModal, LinkContainer, Title, LongUrl, ShortLinkArea, ShortUrl } from './styles'
+
+export default function CustomModal({closeModal}) {
+
+  return (
+    <ModalContainer>
+          <View style={{flex:1}}>
+            <Container>
+
+              <HeaderModal>
+
+                <TouchableOpacity onPress={closeModal}>
+                  <Icon name='close' size={30} color={'#5C1374'}/>
+                </TouchableOpacity>
+
+                <TouchableOpacity>
+                  <IconF name='send' size={28} color={'#5C1374'}/>
+                </TouchableOpacity>
+
+              </HeaderModal>
+
+              <LinkContainer>
+
+                <Title>
+                  Link Encurtado
+                </Title>
+
+                <LongUrl>
+                  kkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkk
+                </LongUrl>
+
+                <ShortLinkArea activeOpacity={1}>
+                  <ShortUrl numberOfLines={1}>
+                    kkkkkkkkkkkkkkk
+                  </ShortUrl>
+                  <IconCopy name='copy' size={20} color={'#5C1374'}/>
+                </ShortLinkArea>
+
+              </LinkContainer>
+
+            </Container>
+          </View>
+    </ModalContainer>
+  )
+}
