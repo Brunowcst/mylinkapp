9ff0ac38dd76f877c8f29649970323fc5f54af82
brunowcst <brunoporfirio@gmail.com>
2023-09-16 21:45:31 -0300

    Update MenuBar

---
+++ mylink/src/components/Menu/index.js
@@ -0,0 +1,21 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+import { ButtonMenu, ContentMenu } from './styles'
+import { useNavigation } from '@react-navigation/native';
+
+import Icon from 'react-native-vector-icons/Ionicons';
+
+export default function index() {
+    const navigation = useNavigation();
+
+  return (
+    <ContentMenu>
+      <ButtonMenu onPress={() => navigation.openDrawer()}>
+        <Icon name='menu' size={40} color={'#5C1374'}/>
+      </ButtonMenu>
+      <ButtonMenu>
+        <Icon name='share-social' size={40} color={'#5C1374'}/>
+      </ButtonMenu>
+    </ContentMenu>
+  )
+}
