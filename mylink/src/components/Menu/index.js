381a11774cd62304354f7d96cd4f2eb7778d2ec7
Brunowcst <brunoporfirio022@gmail.com>
2023-09-17 17:53:43 -0300

    update

--- mylink/src/components/Menu/index.js
+++ mylink/src/components/Menu/index.js
@@ -5,7 +5,7 @@ import { useNavigation } from '@react-navigation/native';
 
 import Icon from 'react-native-vector-icons/Ionicons';
 
-export default function index() {
+export default function Menu() {
     const navigation = useNavigation();
 
   return (
