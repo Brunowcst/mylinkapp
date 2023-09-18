f42250c990d4c9a3d0656465761377cfdab93903
Brunowcst <brunoporfirio022@gmail.com>
2023-09-18 19:31:54 -0300

    mylinks page

--- mylink/src/pages/MyLinks/index.js
+++ mylink/src/pages/MyLinks/index.js
@@ -1,10 +1,32 @@
 import { View, Text } from 'react-native'
 import React from 'react'
 
+import Menu from '../../components/Menu'
+import StatusBarPage from '../../components/StatusBar'
+import ListItem from '../../components/ListItem'
+
+import { Container, Title, ListLinks } from './styles'
+
 export default function MyLinks() {
   return (
-    <View>
-      <Text>MyLinks</Text>
-    </View>
+    <Container>
+      <Menu/>
+      <StatusBarPage 
+        barStyle='light-content' 
+        backgroundColor='#222'
+      />
+
+      <Title>
+        Meus Links
+      </Title>
+
+      <ListLinks
+        data={[{id: 1, link: 'teste.com'}, {id: 2, link: 'teste.comadasdf'}]}
+        keyExtractor={ (item) => String(item.id)}
+        renderItem={ ({item}) => <ListItem name={item.link}/>}
+        showsHorizontalScrollIndicator={false}
+      />
+
+    </Container>
   )
 }
