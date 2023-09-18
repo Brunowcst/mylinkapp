5a557a6d4b2a939f56d350ba7d6a9aeee9df8854
Brunowcst <brunoporfirio022@gmail.com>
2023-09-18 19:32:04 -0300

    styles mylink page

---
+++ mylink/src/pages/MyLinks/styles.js
@@ -0,0 +1,18 @@
+import styled from "styled-components/native";
+
+export const Container = styled.View`
+    flex: 1;
+    background-color: #222;
+`;
+
+export const Title = styled.Text`
+    margin-top: 40px;
+    color: #fff;
+    font-weight: bold;
+    font-size: 30px;
+    padding-left: 20px;
+`;
+
+export const ListLinks = styled.FlatList`
+    margin-top: 40px;
+`;
