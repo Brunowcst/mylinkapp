22262f2bad57f04f14363b4a14f9f9e49135575b
Brunowcst <brunoporfirio022@gmail.com>
2023-09-18 19:31:41 -0300

    styles link component

---
+++ mylink/src/components/ListItem/styles.js
@@ -0,0 +1,21 @@
+import styled from "styled-components/native";
+
+export const Container = styled.TouchableOpacity`
+    background-color: #5C1374;
+    padding: 0px 20px;
+    width: 85%;
+    padding: 15px;
+    align-self: center;
+    border-radius: 10px;
+    flex-direction: row;
+    align-items: center;
+    flex: 1;
+    gap: 15px;
+    margin-bottom: 20px;
+`;
+
+export const Link = styled.Text`
+    flex: 1;
+    color: #fff;
+    font-size: 18px;
+`;
