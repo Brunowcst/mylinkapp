048fad11aaeaefcdf30a984af3720f0a51a16c16
Brunowcst <brunoporfirio022@gmail.com>
2023-09-21 23:11:02 -0300

    selectedItem

--- mylink/src/components/ListItem/index.js
+++ mylink/src/components/ListItem/index.js
@@ -5,9 +5,9 @@ import { Container, Link } from './styles'
 
 import Icon from 'react-native-vector-icons/Feather'
 
-export default function ListItem({data}) {
+export default function ListItem({data, selectedItem}) {
   return (
-    <Container activeOpacity={.8}>
+    <Container activeOpacity={.8} onPress={() => selectedItem(data)}>
         <Icon name='link' color={'#fff'} size={18} />
 
         <Link numberOfLines={1}>
