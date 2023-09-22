697137b8e4442d956dd0208359704bc08d3594b9
Brunowcst <brunoporfirio022@gmail.com>
2023-09-21 23:00:41 -0300

    update props

--- mylink/src/components/ListItem/index.js
+++ mylink/src/components/ListItem/index.js
@@ -5,13 +5,13 @@ import { Container, Link } from './styles'
 
 import Icon from 'react-native-vector-icons/Feather'
 
-export default function ListItem({name}) {
+export default function ListItem({data}) {
   return (
     <Container activeOpacity={.8}>
         <Icon name='link' color={'#fff'} size={18} />
 
         <Link numberOfLines={1}>
-          {name}
+          {data.long_url}
         </Link>
 
     </Container>
