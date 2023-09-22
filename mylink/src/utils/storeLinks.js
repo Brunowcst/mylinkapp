b7e3724a7c642ca16f70111bc17e75f3d4a0f1a4
Brunowcst <brunoporfirio022@gmail.com>
2023-09-21 22:59:27 -0300

    crud links

---
+++ mylink/src/utils/storeLinks.js
@@ -0,0 +1,34 @@
+import AsyncStorage from '@react-native-async-storage/async-storage';
+
+export async function getLinks(key) {
+    const myLinks = await AsyncStorage.getItem(key);
+    let linksSaves = JSON.parse(myLinks) || [];
+    return linksSaves;
+}
+
+export async function saveLinks(key, newLink) {
+    let linksStoraged = await AsyncStorage.getItem(key)
+    
+    if (linksStoraged === null || linksStoraged === undefined) {
+        linksStoraged = [];
+    } else {
+        linksStoraged = JSON.parse(linksStoraged);
+    }
+
+    //Verifica se pelo menos algum link salvo na lista ja possui id que está sendo registrado
+    const hasLink = linksStoraged.some(link => link.id === newLink.id)
+
+    if(hasLink){
+        console.log('Este link já existe.')
+        return
+    } 
+
+    linksStoraged.push(newLink)
+    await AsyncStorage.setItem(key, JSON.stringify(linksStoraged));
+    console.log('Link adicionado.')
+    console.log(linksStoraged)
+}
+
+export async function deleteLinks(links, id) {
+    
+}
