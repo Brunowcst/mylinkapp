49c595ec389ff4f30aaeabe8db70805137d037cf
Brunowcst <brunoporfirio022@gmail.com>
2023-09-21 23:35:52 -0300

    add gesture handler

--- mylink/src/components/ListItem/index.js
+++ mylink/src/components/ListItem/index.js
@@ -1,19 +1,29 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import React from 'react';
+import Swipeable from 'react-native-gesture-handler/Swipeable';
 
-import { Container, Link } from './styles'
+import { Container, Link, ActionContainer } from './styles'
 
 import Icon from 'react-native-vector-icons/Feather'
+import IconTrash from 'react-native-vector-icons/Ionicons'
 
-export default function ListItem({data, selectedItem}) {
+export default function ListItem({data, selectedItem, handleDelete}) {
+
+  function rightAction() {
+    return (
+      <ActionContainer>
+        <IconTrash name='trash' size={25} color={'#fff'}/>
+      </ActionContainer>
+    )
+  }
+
   return (
-    <Container activeOpacity={.8} onPress={() => selectedItem(data)}>
-        <Icon name='link' color={'#fff'} size={18} />
-
-        <Link numberOfLines={1}>
-          {data.long_url}
-        </Link>
-
-    </Container>
+      <Swipeable renderRightActions={rightAction}>
+        <Container activeOpacity={.8} onPress={() => selectedItem(data)}>
+            <Icon name='link' color={'#fff'} size={18} />
+            <Link numberOfLines={1}>
+              {data.long_url}
+            </Link>
+        </Container>
+      </Swipeable>
   )
 }
